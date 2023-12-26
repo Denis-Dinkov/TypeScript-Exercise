df6b6a711c5d76ab322e2529e6be979483f014d8
Denis Dinkov <denisdinkov7@gmail.com>
2023-12-26 17:14:04 +0200

    Update app.ts

--- Classes&Interfaces/src/app.ts
+++ Classes&Interfaces/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,15 +9,19 @@ class Department {
   describe(this: Department) {
     console.log(`Department: ${this.name}`);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmplyees(this: Department) {
+    console.log(this.employees);
+    console.log(this.employees.length);
+  }
 }
 
 const accounting = new Department("Accounting");
-
-// accounting.describe();
-
-const accountingCopy = {
-  name: "dasda",
-  age: "312312321",
-  describe: accounting.describe,
-};
-accountingCopy.describe();
+accounting.addEmployee("Max");
+accounting.printEmplyees();
+accounting.addEmployee("Wanko");
+accounting.printEmplyees();
