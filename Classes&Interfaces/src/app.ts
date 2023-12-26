07d6c76dec75b22a16a3e841a630ff4769b01025
Denis Dinkov <denisdinkov7@gmail.com>
2023-12-26 11:16:30 +0200

    This in classes

--- Classes&Interfaces/src/app.ts
+++ Classes&Interfaces/src/app.ts
@@ -1,1 +1,22 @@
-// Code goes here!
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log(`Department: ${this.name}`);
+  }
+}
+
+const accounting = new Department("Accounting");
+
+// accounting.describe();
+
+const accountingCopy = {
+  name: "dasda",
+  age: "312312321",
+  describe: accounting.describe,
+};
+accountingCopy.describe();
