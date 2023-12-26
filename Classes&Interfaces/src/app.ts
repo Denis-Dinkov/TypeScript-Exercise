33f7128b1858c1c067824909c954a64459ed7edd
Denis Dinkov <denisdinkov7@gmail.com>
2023-12-26 17:26:42 +0200

    Update app.ts

--- Classes&Interfaces/src/app.ts
+++ Classes&Interfaces/src/app.ts
@@ -18,6 +18,16 @@ class Department {
     console.log(this.employees);
     console.log(this.employees.length);
   }
+  pepsiHello() {
+    console.log("I'm Pepsi the Pepilota");
+  }
+
+  reverseWords(sentence: string) {
+    const words = sentence.split(" ");
+    const reversedWords = words.reverse();
+    const reversedSentence = reversedWords.join(" ");
+    console.log(reversedSentence);
+  }
 }
 
 const accounting = new Department("Accounting");
@@ -25,5 +35,5 @@ accounting.addEmployee("Max");
 accounting.printEmplyees();
 accounting.addEmployee("Wanko");
 accounting.printEmplyees();
-
+accounting.reverseWords("Pepsi the pepilota");
 // accounting.employees[3] = "Ne"; // very bad practice
