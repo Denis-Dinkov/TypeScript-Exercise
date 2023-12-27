d98a2faf197a27bcc5e702e8537693ec75eba33f
Denis Dinkov <denisdinkov7@gmail.com>
2023-12-27 15:37:30 +0200

    Protected atribute

--- Classes&Interfaces/src/app.ts
+++ Classes&Interfaces/src/app.ts
@@ -1,7 +1,7 @@
 class Department {
   // private readonly id: string;
   // private name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
@@ -38,6 +38,12 @@ class AcountingDepartment extends Department {
     super(id, "accounting");
   }
 
+  addEmployee(name: string) {
+    if (name == "Max") return;
+
+    this.employees.push(name);
+  }
+
   addReport(text: string) {
     this.reports.push(text);
   }
@@ -54,4 +60,5 @@ console.log(it);
 const accounting = new AcountingDepartment("d2", []);
 
 accounting.addReport("something went wrong");
-accounting.printReports();
+accounting.addEmployee("Vanio");
+console.log(accounting);
